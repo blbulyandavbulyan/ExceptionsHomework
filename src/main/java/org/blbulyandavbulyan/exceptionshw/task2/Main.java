fe1180ab0e140161c487e5cf020b3d0313f722a9
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-27 12:05:47 +0400

    Solution for task 2 fixed, now DivisionByZeroException accepts message in constructor

--- src/main/java/org/blbulyandavbulyan/exceptionshw/task2/Main.java
+++ src/main/java/org/blbulyandavbulyan/exceptionshw/task2/Main.java
@@ -12,11 +12,11 @@ public class Main {
         try{
             System.out.printf("Результат деления: %f", divide(a, b));
         } catch (DivisionByZeroException e) {
-            System.out.println("Деление на ноль недопустимо");
+            System.out.println(e.getMessage());
         }
     }
     public static double divide(double a, double b) throws DivisionByZeroException {
-        if(b == 0)throw new DivisionByZeroException();
+        if(b == 0)throw new DivisionByZeroException("Деление на ноль недопустимо");
         return a/b;
     }
 }
